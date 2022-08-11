dc241c59d92c7f3e1f507e5af0994d704c2f7a31
Andrew Hobson <79083123+hobbo2@users.noreply.github.com>
2022-08-11 16:54:25 +0200

    Create nutrition.py

---
+++ nutrition.py
@@ -0,0 +1,36 @@
+
+
+fruits = {
+    "apple": 130,
+    "avocado" : 50,
+    "banana" : 110,
+    "cantaloupe" : 50,
+    "grapefruit" : 60,
+    "grapes" : 90,
+    "honeydew melon" : 50,
+    "kiwifruit" : 90,
+    "lemon" : 15,
+    "lime" : 20,
+    "Nectarine" : 60,
+    "orange" : 80,
+    "peach" : 60,
+    "pear" : 100,
+    "pineapple" : 50,
+    "plums" : 70,
+    "strawberries" : 50,
+    "sweet cherries" : 100,
+    "tangerine" : 50,
+    "watermelon" : 80}
+
+
+while True:
+    n = input('Item: ')
+    n = n.lower()
+    if n in fruits:
+        break
+
+
+calories = fruits[n]
+print('calories:', calories)
+
+
